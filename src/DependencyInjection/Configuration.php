61e554dbbb51d268f81ef26adab2880971b48f61
Gary PEGEOT <garypegeot@gmail.com>
2018-11-04 18:01:02 +0000

    Fix deprecation message on SF 4.2

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,18 +17,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('miaoucorp_fixture_loader');
+        $treeBuilder = new TreeBuilder('miaoucorp_fixture_loader');
+        $rootNode = \method_exists($treeBuilder, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('miaoucorp_fixture_loader');
         $rootNode->children()
             ->scalarNode('directory')
                 ->cannotBeEmpty()
                 ->defaultValue('%kernel.project_dir%/tests/Resources/fixtures')
                 ->end()
             ->end();
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
 
         return $treeBuilder;
     }
